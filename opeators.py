392a2c7f8cd7e89602e94ee495eaa2d7488c4ad4
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-27 15:50:31 +0400

    Operator in Python

--- opeators.py
+++ opeators.py
@@ -2,16 +2,21 @@
 
 # Mathematical Operator :  +, -, *, /
 
+num1 = 20
+num2 = 5
+print(num1+num2) # Add
+print(num1-num2) # Subtraction
+print(num1*num2) # Multiplication
+print(num1/num2) # Division
 
-# Addition
-num1 = 10
-num2 = 20
-print(num1+num2)
 
-# Addition with User Input
+# User Input
 
 a = int(input("Enter First Number: "))
 b = int(input("Enter Second Number: "))
 
 print("Your Sum is: ", a + b)
+print("Your Sub is: ", a - b)
+print("Your Mul is: ", a * b)
+print("Your Div is: ", a / b)
 
