5a684e2b0aa741fe1c958a072fbb676e634a1359
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-27 15:46:22 +0400

    Opearator in Python

---
+++ opeators.py
@@ -0,0 +1,17 @@
+# In Python, there are many operators I'll share all operators with examples.
+
+# Mathematical Operator :  +, -, *, /
+
+
+# Addition
+num1 = 10
+num2 = 20
+print(num1+num2)
+
+# Addition with User Input
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+print("Your Sum is: ", a + b)
+
