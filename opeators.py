a28defcb99309f1571413f532bcaa29a22c0ce10
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-28 12:42:12 +0400

    All Operators in Python

--- opeators.py
+++ opeators.py
@@ -2,21 +2,55 @@
 
 # Mathematical Operator :  +, -, *, /
 
-num1 = 20
-num2 = 5
-print(num1+num2) # Add
-print(num1-num2) # Subtraction
-print(num1*num2) # Multiplication
-print(num1/num2) # Division
+
+# num1 = 20
+# num2 = 5
+# print(num1+num2) # Add
+# print(num1-num2) # Subtraction
+# print(num1*num2) # Multiplication
+# print(num1/num2) # Division
+
+# # User Input
+
+# a = int(input("Enter First Number: "))
+# b = int(input("Enter Second Number: "))
+
+# print("Your Sum is: ", a + b)
+# print("Your Sub is: ", a - b)
+# print("Your Mul is: ", a * b)
+# print("Your Div is: ", a / b)
+
+# # Assignment Operator : =, +=, -=
+
+# a = 5  # I'm assigning 5 to a.
+# a += 10  # adding 10 in 5(a)
+# a -= 2  # subtracting 2 in a(5)
+# print(a)
+
+# Comparison Operator: ==, >, <, >=, <=, !=
+
+# a = 10
+# b = 15
+
+# print(a == b) # here I'm comparing a is equal to b, so the output is False
+# print(a > b) #Fasle
+# print(a < b) #True
+# print(a >= b) #False
+# print(a <= b) #True
+# print(a != b) #True
 
 
-# User Input
+# Logical Operator : and, or, not
 
-a = int(input("Enter First Number: "))
-b = int(input("Enter Second Number: "))
+# a = 10
+# b = 20
+# c = 10
 
-print("Your Sum is: ", a + b)
-print("Your Sub is: ", a - b)
-print("Your Mul is: ", a * b)
-print("Your Div is: ", a / b)
+# print(a==b and a==c) # and is basically to correct all the statement
+# print(a==b or a==c) # or is basically to correct anyone of the statement
+
+# print(not(True)) # not is reversal True to False => False to True
+
+
+
 
