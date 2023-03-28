1af99ee0106bef70bb5574cb2397d745d8efb19e
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-28 14:29:41 +0400

    Practice for Lists & Tuple

---
+++ practice3.py
@@ -0,0 +1,38 @@
+# write a program to store seven fruits in a list entered by the user
+# write a program to accept marks of 6 students and display them in a sorted manner
+# check that a tuple cannot be changed in python
+# write a program to sum a list with 4 numbers
+# write a program to count the number of zeros in the following tuple
+# a = (7, 0, 8, 0, 0, 9)
+
+
+# write a program to store seven fruits in a list entered by the user
+
+# a = input("Enter number: ")
+# list = []
+# list.append(a)
+# print(list)
+
+# write a program to accept marks of 6 students and display them in a sorted manner
+# marks = [84, 67, 99, 73, 93, 82]
+# marks.sort()
+# print(marks)
+
+# check that a tuple cannot be changed in python
+# l = (3, 5, 6)
+# l[1] = 9
+# l.append(1)
+# print(l) # we are getting error when we are appending or changing value thats mean tuple cannot be changed.
+
+# write a program to sum a list with 4 numbers
+# list = [2, 4, 6, 8]
+# sum = 0
+# for i in list:
+#     sum += i
+# print(sum)
+
+
+# write a program to count the number of zeros in the following tuple
+# a = (7, 0, 8, 0, 0, 9)
+# print(a.count(0))
+
