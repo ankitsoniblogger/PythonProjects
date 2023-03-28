e8f78059c721f8a73614e8d91daa98e981dcd0f3
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-28 13:43:14 +0400

    escape characters and string practices set

---
+++ pracitce2.py
@@ -0,0 +1,32 @@
+# write a python program to display a user entered name followed by Good Afternoon using input() function
+
+# str = str(input("Enter your Name: "))
+
+# print("Good Afternoon",str,"!")
+
+# Write a program to fill in a letter template given below with name and date
+
+# letter = '''Dear Ankit,
+# you are selected!
+# 28-03-2023.'''
+# print(letter)
+
+# write a program to detect double space in a strings.
+
+# str = 'Ankit Soni'
+#
+# if(str.count('  ')):
+#     print("Yes, there is a double space")
+# else:
+#     print("Fine!")
+
+# Replace to double space from given string "Ankit  Soni" with single space
+
+# str= 'Ankit Soni'
+# new_str = str.replace('  ', ' ')
+# print(new_str)
+
+# Write a program to format the following letter using escape sequence char
+
+# letter = "Hello All, \n Welcome to Python Beginners Practice Question, \n Thanks"
+# print(letter)
