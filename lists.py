58d48cbd8d7a7f8ac06cac3d8b62fae2835b8cc8
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-28 13:56:27 +0400

    Lists in Python

---
+++ lists.py
@@ -0,0 +1,15 @@
+# f = ['Ankit', 'Ankur', 'Durgesh', 'Amit', 'Abhishek', 'Shivam']
+# print(type(f))
+# print(f)
+# print(len(f)) # length of list
+
+# f.append('Akash')
+# print(f)
+# f.pop(5)
+# print(f)
+
+
+# l = [3,5,6,1,6,23]
+# l.sort()
+# l.reverse()
+# print(l)
