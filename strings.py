d82c3989cdc4d064ce58b10733bc27f292ce8305
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-28 13:23:40 +0400

    Basic Strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+# a = 'Ankit' # single quoted strings
+# b = "Ankit"  # double quoted strings
+# c = '''Ankit''' # tripe quoted strings
+#
+# print(a, b, c)
+
+
+# str = "Ankit Soni"
+# print(len(str)) #Check the length of given strings
+# print(str[0:6]) #slicing string from 0 index position to n-1 position
+# print(str[6:10]) #slicing string from 0 index position to n-1 position
+
+# print(str.count('n')) # count char
+
+# print(str.upper()) # convert to UPPERCASE
