b1fccdd4f4713e36a0632978f9dcb87ed08fbade
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-28 12:48:29 +0400

    if, elif, else statement in Python

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+# if, else mean it's only for condition checking, we can check any condition using if else and elif
+
+num = int(input("Enter Age: "))
+
+if(num > 18):
+    print("Hurray! You're a Man.")
+elif(num==18):
+    print("It's your day Bro!")
+else:
+    print("Still, you're a Kid")
