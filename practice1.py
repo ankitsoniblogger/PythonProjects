7657b84ef1782858e5c3731fb66dd32c1cabe16a
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-28 13:13:35 +0400

    6 Basic Python Practice Questions...

---
+++ practice1.py
@@ -0,0 +1,44 @@
+# Write a Python program to add two number
+# Write a Python Program to find remainder when a number is divided by z.
+# Check the type of the variable assigned using input() function.
+# Use comparison operator to find out weather a given variable a is grater then b or not take a = 34 and b = 80
+# Write a Python Program to find average of two number entered by the user.
+# Write a python program to calculate square of a number entered by the user.
+
+
+
+# Write a Python program to add two number
+
+# a = int(input("Enter first Number: "))
+# b = int(input("Enter second Number: "))
+# print(a+b)
+
+# Write a Python Program to find remainder when a number is divided by z.
+
+# z = 2
+# print(11%z)
+
+# Check the type of the variable assigned using input() function.
+
+# a = input("Type Anything...")
+# print(type(a), a)
+
+# Use comparison operator to find out weather a given variable a is grater then b or not take a = 34 and b = 80
+
+# a = 34
+# b = 80
+# print(a>b)
+
+# Write a Python Program to find average of two number entered by the user.
+
+# num1 = int(input("Enter 1st Number: "))
+# num2 = int(input("Enter 2nd Number: "))
+
+# avg = (num1 + num2)/2
+
+# print(avg)
+
+# Write a python program to calculate square of a number entered by the user.
+
+# num = int(input("Enter Number: "))
+# print(num**2)
