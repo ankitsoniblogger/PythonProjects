1cc407aa4ed9b887135dee5ba7627d89460ac26e
Ankit soni <ankitsoniblogger@gmail.com>
2023-03-29 10:23:24 +0400

    Dictionary & Sets

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# Dictionary is a key value pairs e.g
+dict = {
+    'name': 'Ankit',
+    'company': 'Akbar Travels',
+    'role': 'WordPress Developer',
+    'location': 'Dubai'
+}
+
+# print(dict.keys())  # all keys
+# print(dict.values()) # all values
+# print(dict.items()) # all items
+# print(dict.get('role')) # get particular value with the help of key
+# dict.clear() # clear the dict
+# dict.update ( {"age": 23}) # add value
+print(dict)
---
+++ sets.py
@@ -0,0 +1,15 @@
+# s = set() # empty set
+# print(type(s))
+# print(s)
+
+# s = {1, 8, 4, 7}
+# m = {2,3,5, 4}
+# print(len(s))
+
+# s.remove(8)
+
+# print(s.union(m)) # all elements avoid duplicate
+# print(s.intersection(m)) # common elements only
+
+# s = set()
+# print(len(s))
